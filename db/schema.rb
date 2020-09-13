3ed3823fdeb2343bfc4f4be84cbf109a1923873c
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-13 08:54:49 +0000

    [Finish]Topページ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_041220) do
+ActiveRecord::Schema.define(version: 2020_09_13_083928) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profile_images", force: :cascade do |t|
     t.integer "user_id"
