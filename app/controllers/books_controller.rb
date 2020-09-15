0ac82117040ffdac1601241d1fa2b3623fcc8956
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-15 05:55:30 +0000

    [Finish]編集機能修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,8 +26,8 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.new(book_params)
-    @book.save
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to book_path(@book.id)
   end
 
