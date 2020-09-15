33da5ca71e964709a272e599bb275420e04d4e7e
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-15 05:29:51 +0000

    [Finish]編集削除機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,13 +18,24 @@ class BooksController < ApplicationController
   end
 
   def show
-    # https://console.aws.amazon.com/cloud9/ide/5c33c9583ae945ebae93a998f357e435/books/:id
-    # https://console.aws.amazon.com/cloud9/ide/5c33c9583ae945ebae93a998f357e435/books/1
-
     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+     flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
 private
