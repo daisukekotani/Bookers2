952bfee3b9697ced2bb165a10cbe2a14b59ea482
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-17 11:47:37 +0000

    [Finish]デバイスヘッダー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(resource.id)
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path # ←redirect先にしたいpathを自分で書く
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   resources :post_images, only: [:new, :create, :index, :show]
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
