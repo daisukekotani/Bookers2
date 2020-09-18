40f7de334edba21b0ce78b8ef9d1f34404eda75a
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-18 08:41:24 +0000

    [Finish]サインアップ時のエラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :profile_images, dependent: :destroy
   attachment :profile_image
+
+  validates :name, {presence: true}
+  validates :name, length: { in: 2..20 }  # 「2文字以上20文字以下はture」
+
 end
