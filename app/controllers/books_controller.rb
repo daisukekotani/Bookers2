4d531e1777892b3e7722ca020c666ae01cd480c6
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-19 10:40:17 +0000

    [Finish] Bootstrap3格ページ編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,8 +20,8 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
     @book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def edit
